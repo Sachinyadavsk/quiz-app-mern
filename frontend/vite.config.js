ec48b183ce3b3eb9c6fcb10bf708a6c71d0d7b8f
Sachinyadavsk <sachinsk4832499@gmail.com>
2025-12-09 16:52:27 +0530

    route issue the solve

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    base: '/quiz-app-mern/', // Replace with your repository name
 })
